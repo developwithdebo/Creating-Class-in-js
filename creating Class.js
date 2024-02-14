8a109aecaa56149b6c4cd5b1425c684df7dcad91
Adesanya Abdullateef Adebowale <debo@abdullateet.dev>
2024-02-14 21:33:04 +0100

    Add files via upload

---
+++ creating Class.js
@@ -0,0 +1,12 @@
+class Train{
+    constructor(color, lightsOn) {
+        this.color = color;
+        this.lightsOn = lightsOn;
+    
+    }
+}
+new Train()
+var myFirstTrain = new Train('red', false)
+var mySecondTrain= new Train('red', false)
+console.log(myFirstTrain)
+console.log(mySecondTrain)
